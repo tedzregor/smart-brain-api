eff04d12168176c344d83274ca4df54c07482320
tedzregor <tedzzzregor@gmail.com>
2022-05-01 20:15:20 +0800

    adding port

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-  console.log(data);
-});
+// db.select('*').from('users').then(data => {
+//   console.log(data);
+// });
 
 app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
@@ -36,6 +36,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req,res,db)})
 app.put('/image', (req,res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, ()=> {
-  console.log(`app is running on port`);
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
